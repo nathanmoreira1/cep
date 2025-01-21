a40152c7120115464a621f5745dea2881345920c
Nathan Moreira de Macedo <nathanmoreirademacedo@gmail.com>
2025-01-21 11:40:03 -0300

    fix traducao

--- bootstrap.php
+++ bootstrap.php
@@ -5,15 +5,12 @@ use App\Factories\CepServiceFactory;
 use App\Factories\ControllerFactory;
 use App\Contracts\CepServiceInterface;
 
-// Criar o container
 $container = new ServiceContainer();
 
-// Registrar o serviço de CEP
 $container->register(CepServiceInterface::class, function () {
-    return CepServiceFactory::create();  // A fábrica lida com a criação do serviço
+    return CepServiceFactory::create();
 });
 
-// Registrar a ControllerFactory
 $container->register('controller_factory', function () use ($container) {
     return new ControllerFactory($container);
 });
